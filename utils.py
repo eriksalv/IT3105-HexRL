e799d34e5d46c2d91a4ec70ecdc7a596f67580e7
Jesper Elverum <jesper240800@gmail.com>
2024-04-10 15:15:07 +0200

    added examples to show the generate_more_cases + bugfix

--- utils.py
+++ utils.py
@@ -4,14 +4,12 @@ from games.hex import HexStateManager, Player
 
 
 def generate_more_cases(state: np.ndarray, distribution: np.ndarray, player_value, k):
-    #print(state)
-    #print(distribution)
-    #print(player_value)
+
     cases = [(np.append(state.flatten(), player_value), distribution)]
 
     #generate state for opponent
-    opponent_state = generate_blue_state(state)
-    opponent_distribution = generate_blue_distribution(k, distribution)
+    opponent_state = generate_opponent_state(state)
+    opponent_distribution = generate_opponent_distribution(k, distribution)
     if player_value == 1: opponent_value = 2 
     else: opponent_value = 1
     cases.append((np.append(opponent_state, opponent_value), opponent_distribution))
@@ -42,36 +40,59 @@ def rotate_distribution(k: int,distribution: np.ndarray) -> np.ndarray:
     rotated_distribution = rotated_distribution_matrix.flatten()
     return rotated_distribution
 
-def generate_blue_state(board):
+def generate_opponent_state(board):
+    print(board)
     rotated_board = np.transpose(board)[:, ::-1]
     conditions = [rotated_board == 1, rotated_board == 2]
     choices = [2, 1]  # The choices to swap
     swapped_board = np.select(conditions, choices, default=rotated_board)
+    print(swapped_board)
     
     return swapped_board.flatten()
 
-def generate_blue_distribution(k: int, distribution: np.ndarray) -> np.ndarray:
+def generate_opponent_distribution(k: int, distribution: np.ndarray) -> np.ndarray:
     distribution_2d = distribution.reshape((k, k))
+    print(distribution_2d)
+
     rotated_distribution = np.transpose(distribution_2d)[:, ::-1]
-    
+    print(rotated_distribution)
     transformed_distribution = rotated_distribution.flatten()
 
     return transformed_distribution
 
 if __name__ == '__main__':
+    print('Example 1 - shows that the move to be made as blue will translate to the move that should be made by red')
     k = 3
     hsm = HexStateManager(k)
     hsm.new_game()
-    hsm.make_move((0, 0))
-    hsm.make_move((2, 0))
-    hsm.make_move((0, 1))
-    hsm.make_move((1, 0))
-    hsm.make_move((1, 1))
-    hsm.make_move((1, 2))
-    hsm.make_move((2 ,1)) 
+    hsm.make_move((0, 0)) #red
+    hsm.make_move((2, 0)) #blue
+    hsm.make_move((0, 1)) #red
+    hsm.make_move((1, 0)) #blue
+    hsm.make_move((1, 1)) #red
+    
+    
 
-    hsm.show_board()
+    #hsm.show_board()
 
 
-    generate_more_cases(hsm.board, np.array([1,0,0,0,0,0,0,0,0]), k)
+    generate_more_cases(hsm.board, np.array([0,0,0,0,0,0,0,1,0]), hsm.current_player.value, k)
+
     #print(np.flip(hsm.board.flatten(), axis=0)) # faster? seems like this performs the same operation
+
+    print('Example 2 - shows that the move to be made as red will translate to the move that should be made by blue')
+
+    k = 3
+    hsm = HexStateManager(k)
+    hsm.new_game()
+    hsm.make_move((0, 0)) #red
+    hsm.make_move((2, 0)) #blue
+    hsm.make_move((0, 1)) #red
+    hsm.make_move((1, 0)) #blue
+    hsm.make_move((1, 1)) #red
+    hsm.make_move((2, 1)) #blue
+
+    #hsm.show_board()
+
+
+    generate_more_cases(hsm.board, np.array([0,0,0,0,0,0,0,0,1]), hsm.current_player.value, k)
